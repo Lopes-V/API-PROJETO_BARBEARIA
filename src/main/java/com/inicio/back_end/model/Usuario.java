02bc42bb350b5fe1afaff10483df61624824328a
Vinícius Lopes <lopeavini@gmail.com>
2026-03-26 07:39:58 -0300

    chore(security): iniciar configuracao de seguranca

---
+++ src/main/java/com/inicio/back_end/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.inicio.back_end.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+    @Override
+    public String getPassword() { return senha; }
+    @Override
+    public String getUsername() { return login; }
+}
---
+++ src/main/java/com/inicio/back_end/service/ServiceAutenticacao.java
@@ -0,0 +1,19 @@
+package com.inicio.back_end.service;
+
+import com.inicio.back_end.repository.RepositoryUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceAutenticacao {
+
+
+        private RepositoryUsuario repository;
+
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            return repository.findByLogin(username);
+
+    }
+}
